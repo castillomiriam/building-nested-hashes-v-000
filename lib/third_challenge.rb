50f145aec9a7fcc5871b0eb1829cab077ded8584
Miriam Castillo <castillo.m.mc@gmail.com>
2018-07-22 22:13:10 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,33 @@
-epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => []
-   }
-}
+def third_challenge
+  def third_challenge
+    epic_tragedy = {
+     :montague => {
+       :patriarch => {
+       :patriarch => {name: "Lord Montague", age: "53"
+          #your key/value pairs here
+          },
+       :matriarch => {
+       :matriarch => {name: "Lady Montague", age: "54"
+          #your key/value pairs here
+          },
+       :hero => {
+       :hero => {name: "Romeo", age: "15", status: "alive"
+          #your key/value pairs here
+          },
+        :hero_friends => []
+     },
+     :capulet => {
+       :patriarch => {
+       :patriarch => {name: "Lord Capulet", age: "50"
+          #your key/value pairs here
+          },
+       :matriarch => {
+       :matriarch => {name: "Lady Capulet", age: "51"
+          #your key/value pairs here
+          },
+       :heroine => {
+       :heroine => {name: "Juliet", age: "15", status: "alive"
+          #your key/value pairs here
+          },
+        :heroine_friends => []
+end

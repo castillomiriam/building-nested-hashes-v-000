2871256d3b7e9ed764da0f43074044dc8a516ab2
Miriam Castillo <castillo.m.mc@gmail.com>
2018-07-22 22:15:06 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,7 +26,7 @@ def third_challenge
           #your key/value pairs here
           },
        :heroine => {
-       :heroine => {name: "Juliet", age: "15", status: "alive"
+        :heroine => {name: "Juliet", age: "15", status: "alive"
           #your key/value pairs here
           },
         :heroine_friends => []
